1efffca58f40d004acdaef22257ecc857f9d1bae
Reinis Mednis <mednisreinis@gmail.com>
2024-07-07 02:20:00 +0300

    [statistics] Initial commit
    This cog is responsible for gathering statistics and inserting them into a influxdb database for long term analysis.

---
+++ statistics/database.py
@@ -0,0 +1,60 @@
+import logging
+
+from influxdb_client import Point
+from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
+
+log = logging.getLogger("red.mednis-cogs.statistics.database")
+
+client: InfluxDBClientAsync
+bucket: str
+org: str
+write_api: InfluxDBClientAsync.write_api
+
+
+async def activate_client(address: str, bucket_str: str, token_str: str, org_str: str):
+    global client, write_api, bucket, org
+
+    client = InfluxDBClientAsync(url=address, token=token_str, org=org_str)
+    write_api = client.write_api()
+
+    bucket = bucket_str
+    org = org_str
+
+    await client.ping()
+
+
+async def write_vc_stats(guild_id: int, channel_id: int, channel_name: str, member_count: int, members: list):
+    id_string = ""
+    name_string = ""
+    for member in members:
+        id_string += f"{member.id},"
+        name_string += f"{member.name},"
+
+    data_point = (
+        Point("voice_channel_stats")
+        .tag("guild_id", guild_id)
+        .tag("channel_id", channel_id)
+        .tag("channel_name", channel_name)
+        .field("member_count", member_count)
+        .field("member_ids", id_string)
+        .field("member_names", name_string)
+    )
+
+    await write_api.write(bucket=bucket, org=org, record=str(data_point))
+
+
+# Write general data points
+async def write_data_point(measurement: str, tag: dict, data: dict):
+    data_point = Point(measurement)
+
+    for tag_key, tag_value in tag.items():
+        data_point = data_point.tag(tag_key, tag_value)
+
+    for data_key, data_value in data.items():
+        data_point = data_point.field(data_key, data_value)
+
+    try:
+        await write_api.write(bucket=bucket, org=org, record=str(data_point))
+    except Exception as e:
+        log.error(f"Failed to write data point: {e}")
+        log.error(f"Data pont: {data_point}")
---
+++ statistics/statistics.py
@@ -0,0 +1,171 @@
+import logging
+from datetime import datetime
+from typing import Literal
+
+import discord
+from discord.ext import tasks
+from redbot.core import commands
+from redbot.core.bot import Red
+from redbot.core.config import Config
+from statistics import database
+
+RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
+log = logging.getLogger("red.mednis-cogs.statistics")
+
+
+class Statistics(commands.Cog):
+    """
+    A cog for pulling server and bot statistics into influx.
+    """
+
+    def __init__(self, bot: Red) -> None:
+        self.bot = bot
+        self.config = Config.get_conf(
+            self,
+            identifier=92651437657460736,
+            force_registration=True,
+        )
+
+        default_bot = {
+            "address": None,
+            "bucket": None,
+            "token": None,
+            "org": None
+        }
+
+        self.config.register_global(**default_bot)
+
+    def cog_load(self) -> None:
+        log.info("Statistics cog loaded")
+
+        self.statistics_gather_loop.start()
+
+    def cog_unload(self) -> None:
+        self.statistics_gather_loop.cancel()
+
+    @tasks.loop(seconds=30, reconnect=True)
+    async def statistics_gather_loop(self):
+        # Gather statistics
+        await self.update_bot_stats()
+        await self.update_all_vc_stats()
+
+        pass
+
+    @statistics_gather_loop.before_loop
+    async def before_statistics_gather_loop(self):
+        log.info("Waiting for bot to be ready...")
+
+        if await self.config.address() is None:
+            log.error("❌ No statistics database address set. "
+                      "Please set one using the set_statistics_db command then restart.")
+            self.statistics_gather_loop.cancel()
+
+        try:
+            await database.activate_client(await self.config.address(), await self.config.bucket(),
+                                           await self.config.token(), await self.config.org())
+        except Exception as e:
+            log.error(f"❌ Failed to connect to statistics database: {e}"
+                      " Statistics will not be gathered.")
+            self.statistics_gather_loop.cancel()
+
+        await self.bot.wait_until_ready()
+
+        log.info("Bot ready, testing database connection...")
+
+        await database.client.ping()
+        log.info("✅ Database up, Statistics will be gathered.")
+
+    @commands.Cog.listener()
+    async def on_statistics_event(self, measurement: str, event: dict, data: dict) -> None:
+        await database.write_data_point(measurement, event, data)
+
+    """
+    Shard/Connection statistics
+    """
+
+    async def update_bot_stats(self):
+
+        if self.bot.uptime is not None:
+            uptime = (datetime.utcnow() - self.bot.uptime).total_seconds()
+        else:
+            uptime = 0
+
+        data = {
+            "latency": self.bot.latency,
+            "guild_count": len(self.bot.guilds),
+            "user_count": len(self.bot.users),
+            "uptime": uptime,
+        }
+
+        await database.write_data_point("bot_stats", {"bot_id": self.bot.user.id}, data)
+
+    """
+    Voice channel statistics
+    """
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self,
+                                    member: discord.Member,
+                                    before: discord.VoiceState,
+                                    after: discord.VoiceState) -> None:
+        guild_id = member.guild.id
+
+        # Update the statistics for the channel the member left
+        if before.channel is not None:
+            channel_id = before.channel.id
+
+            # We cant use the before.channel.members list as the member is already gone
+            channel = member.guild.get_channel(channel_id)
+
+            await database.write_vc_stats(guild_id, channel_id, channel.name, len(channel.members),
+                                          [member for member in channel.members])
+
+        # Update the statistics for the channel the member joined
+        if after.channel is not None:
+            channel_id = after.channel.id
+            await database.write_vc_stats(guild_id, channel_id, after.channel.name, len(after.channel.members),
+                                          [member for member in after.channel.members])
+
+    async def update_all_vc_stats(self):
+        for guild in self.bot.guilds:
+            for channel in guild.voice_channels:
+                await database.write_vc_stats(guild.id, channel.id, channel.name, len(channel.members),
+                                              [member for member in channel.members])
+
+    """
+    Configuration commands
+    """
+
+    @commands.command("set_statistics_db")
+    @commands.dm_only()
+    @commands.is_owner()
+    async def statistics_set(self, ctx: commands.Context, address: str, bucket: str, org: str, token: str):
+        """
+        Set the api credentials for the statistics database
+
+        *address*: The address of the database
+        *bucket*: The bucket to store the data in
+        *org*: The influxdb organization
+        *token*: The key for the database
+        """
+
+        if ctx.author.id not in self.bot.owner_ids:  # Double check just in case xD
+            return await ctx.send("You must be the bot owner to use this command")
+
+        await self.config.address.set(address)
+        await self.config.bucket.set(bucket)
+        await self.config.org.set(org)
+        await self.config.token.set(token)
+
+        await ctx.send("Statistics database credentials set... Testing connection...")
+
+        try:
+            await database.activate_client(address, bucket, token, org)
+            await ctx.send("Connection successful. :D")
+        except Exception as e:
+            await ctx.send(f"Failed to connect to the database: {e}")
+            return
+
+    async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int) -> None:
+        # TODO: Replace this with the proper end user data removal handling.
+        super().red_delete_data_for_user(requester=requester, user_id=user_id)
