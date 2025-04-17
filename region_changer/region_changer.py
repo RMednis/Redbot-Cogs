14198eb2dfd43191d6e8fc8dc404d9c6119f708b
Reinis Mednis <mednisreinis@gmail.com>
2025-04-18 01:26:27 +0300

    [regions] Automatic region list refresh
    If a failed region swap occurs, the region list is now automatically updated to the returned values.
    
    The clean command now also refreshes the region list. :D

--- region_changer/region_changer.py
+++ region_changer/region_changer.py
@@ -1,8 +1,9 @@
+from http.client import HTTPException
 from typing import Literal
 
 import discord
 from discord import app_commands
-from discord.app_commands import Choice
+from discord.app_commands import Choice, Command
 import logging
 from redbot.core import commands
 from redbot.core.bot import Red
@@ -25,17 +26,82 @@ class RegionChanger(commands.Cog):
             force_registration=True,
         )
 
+        default_bot = {
+            "regions": {}
+        }
+
         default_guild = {
             "channel_whitelist": [],
             "enabled": False
         }
 
         self.config.register_guild(**default_guild)
+        self.config.register_global(**default_bot)
 
 
     region_settings = app_commands.Group(name="region_settings",
                                         description="Configure the roles and channels that can change the region of a voice channel",
                                         guild_only=True)
+
+    region_names = {
+        "brazil": "Brazil",
+        "hongkong": "Hong Kong",
+        "india": "India",
+        "japan": "Japan",
+        "rotterdam": "Rotterdam",
+        "russia": "Russia",
+        "singapore": "Singapore",
+        "south-korea": "South Korea",
+        "southafrica": "South Africa",
+        "sydney": "Sydney",
+        "us-central": "US Central",
+        "us-east": "US East",
+        "us-south": "US South",
+        "us-west": "US West"
+    }
+
+    async def update_region_list(self, error: discord.HTTPException) -> None:
+        """
+        Update the region list if the error is a 400 error
+        """
+        if error.status == 400:
+            if "In rtc_region: Value must be one of" in error.text:
+                # Get the list of available regions
+                text = error.text.split("In rtc_region: Value must be one of (")[1].split(",")
+                regions = []
+                for part in text:
+                    part = part.strip()
+                    part = part.replace("'", "")
+                    part = part.replace('(', "")
+                    part = part.replace(')', "")
+                    part = part.replace('.', "")
+                    regions.append(part)
+
+                logger.info(f"Updated region list: {regions}")
+
+                # Make the list into a nicely formated array of choices
+                choices = {}
+                for region in regions:
+                    # If the region is not in the region_names dict, add it as is
+                    if region not in self.region_names:
+                        choices[region] = region
+
+                    # Otherwise, add it with the name from the region_names dict
+                    else:
+                        choices[region] = self.region_names[region]
+
+                # Update the config
+                await self.config.regions.set(choices)
+
+    async def region_autocomplete(self, interaction: discord.Interaction, current: str):
+        # Get config
+        regions = await self.config.regions()
+        # If the config is empty, return the default region names
+        if not regions:
+            regions = self.region_names
+
+        return [Choice(name=regions[region], value=region) for region in regions.keys() if current.lower() in region.lower()]
+
 
     @app_commands.guild_only()
     @region_settings.command(name="enable", description="Enable the region changer")
@@ -51,6 +117,9 @@ class RegionChanger(commands.Cog):
     @app_commands.guild_only()
     @region_settings.command(name="add_channel", description="Add a channel to the whitelist")
     async def add_channel(self, interaction: discord.Interaction, channel: discord.VoiceChannel):
+        if not isinstance(channel, discord.VoiceChannel):
+            return await interaction.response.send_message("The channel must be a voice channel", ephemeral=True)
+
         async with self.config.guild(interaction.guild).channel_whitelist() as channels:
             channels.append(channel.id)
             await self.config.guild(interaction.guild).channel_whitelist.set(channels)
@@ -76,30 +145,35 @@ class RegionChanger(commands.Cog):
             channel_diff = len(channels) - len(channels_new)
             await self.config.guild(guild).channel_whitelist.set(channels_new)
 
+        if (interaction.channel.id not in await self.config.guild(interaction.guild).channel_whitelist() or not
+                isinstance(interaction.channel, discord.VoiceChannel)):
+            region_update_string = "The channel you are in has is not on the whitelist/a voice channel. Did not perform region refresh. \n"
+
+        else:
+            region_update_string = "The channel you are in is on the whitelist. Performing region refresh. \n"
+            try:
+                await interaction.channel.edit(rtc_region="aaaa") # Invalid region to force a refresh
+                region_update_string = "Region refresh failed, somehow the API did not return a 400 error. \n"
+
+            except discord.Forbidden:
+                region_update_string += "I don't have permission to change the region of this channel"
+
+            except discord.HTTPException as e:
+                await self.update_region_list(e)
+                region_update_string += "Region list updated.\n"
+
         await interaction.response.send_message(f"Cleaned the channel whitelist. \n"
-                                                f"Removed {channel_diff} channels")
+                                                f"Removed {channel_diff} channels \n"
+                                                f"{region_update_string}")
 
     @app_commands.guild_only()
-    @app_commands.checks.cooldown(1, 30, key=lambda i: i.user.id)
-    @app_commands.describe(region="The region to change the voice channel to")
-    @app_commands.choices(region=[
-        Choice(name="Brazil", value="brazil"),
-        Choice(name="Hong Kong", value="hongkong"),
-        Choice(name="India", value="india"),
-        Choice(name="Japan", value="japan"),
-        Choice(name="Rotterdam", value="rotterdam"),
-        Choice(name="Russia", value="russia"),
-        Choice(name="Singapore", value="singapore"),
-        Choice(name="South Korea", value="south-korea"),
-        Choice(name="South Africa", value="southafrica"),
-        Choice(name="Sydney", value="sydney"),
-        Choice(name="US Central", value="us-central"),
-        Choice(name="US East", value="us-east"),
-        Choice(name="US South", value="us-south"),
-        Choice(name="US West", value="us-west")
-    ])
+    @app_commands.checks.cooldown(1, 10, key=lambda i: i.user.id)
+    @app_commands.rename(region_name="set")
+    @app_commands.describe(region_name="The region to change the voice channel to")
+    @app_commands.autocomplete(region_name=region_autocomplete)
+
     @app_commands.command(name="region", description="Change the region of the voice channel")
-    async def region(self,interaction: discord.Interaction, region: str):
+    async def region(self,interaction: discord.Interaction, region_name: str = None):
         if not isinstance(interaction.channel, discord.VoiceChannel):
             return await interaction.response.send_message("This command can only be used in a voice channel",
                                                            ephemeral=True)
@@ -111,8 +185,32 @@ class RegionChanger(commands.Cog):
             return await interaction.response.send_message("This channel is not whitelisted", ephemeral=True)
 
         old_region = interaction.channel.rtc_region
-        await interaction.channel.edit(rtc_region=region)
-        await interaction.response.send_message(f"Region of {interaction.channel.mention} changed from `{old_region}` to `{region}`")
+
+        if region_name is None or region_name == "":
+            return await interaction.response.send_message(f"The current region for {interaction.channel.mention} is `{old_region}`",
+                                                           ephemeral=True)
+        # Check if the region is valid
+        if region_name not in await self.config.regions():
+            return await interaction.response.send_message(f"Invalid region `{region_name}`. \n"
+                                                           f"Available regions: `{', '.join(await self.config.regions())}`",
+                                                           ephemeral=True)
+
+        if region_name == old_region:
+            return await interaction.response.send_message(f"The region is already set to `{region_name}`", ephemeral=True)
+
+        try:
+            await interaction.channel.edit(rtc_region=region_name)
+        except discord.Forbidden:
+            await interaction.response.send_message("I don't have permission to change the region of this channel")
+            return
+        except discord.HTTPException as e:
+            await interaction.response.send_message("**Failed to change the region.** \n"
+                                                    "This may be temporary or the list of available regions has changed on discord's side.",
+                                                    ephemeral=True)
+            await self.update_region_list(e)
+            return
+
+        await interaction.response.send_message(f"Region of {interaction.channel.mention} changed from `{old_region}` to `{region_name}`")
 
 
     async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int) -> None:
